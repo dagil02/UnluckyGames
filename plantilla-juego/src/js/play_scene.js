fd9e3f1d397c12b2cf35aad1ba0ae926723c27b7
jossan10 <jossan10@ucm.es>
2019-01-04 21:58:28 +0100

    vuelta a estado anterir de la rama
    Existen conflictos hay que ver como solventarlos antes de mergear

--- plantilla-juego/src/js/play_scene.js
+++ plantilla-juego/src/js/play_scene.js
@@ -58,19 +58,15 @@ var PlayScene = {
     this.pause = false;
     var Pause;
 
-    //Musica
-    var MusicaFondo;
-    this.MusicaFondo = this.game.add.audio('AudioJuego',0.05,true);
-       
-    this.MusicaFondo.play();
-
-    
   },
 
   update: function() {
+   
     if (!this.pause) {
-      this.checkInput();
-      this.playerGroup.children[0].bulletUpdate();
+      this.checkInput(); //gestiona el input de cada jugador en su turno
+      this.playerGroup.children[0].bulletUpdate(this.mapa);  
+    
+       //gestiona las colisiones de balas y su llamada a destrucción
     } else {
       //La tecla enter manda al menu inicial
       if (this.inputAux.isDown(13)) {
@@ -88,7 +84,7 @@ var PlayScene = {
     var y = 38;
 
     this.game.debug.text(
-      `Debugging object: PLAYER TEST`,
+      `PLAYER TESTING: `,
       32,
       610,
       "yellow",
@@ -103,6 +99,25 @@ var PlayScene = {
       "yellow",
       "Segoe UI"
     );
+
+    if (this.playerGroup.children[0].currentWeapon){
+      this.game.debug.text(
+        `Debugging object: CURRENT WEAPON: ` +  this.playerGroup.children[0].currentWeapon.tipoArma,
+        350,
+        610,
+        "yellow",
+        "Segoe UI"
+      );
+      this.game.debug.text(
+        "balas: " + this.playerGroup.children[0].currentWeapon.balas_Cont + " damage: " + 
+        this.playerGroup.children[0].currentWeapon.damage + " alcance: " + this.playerGroup.children[0].currentWeapon.alcance,
+        350,
+        640,
+        "yellow",
+        "Segoe UI"
+      );
+    }
+    
     //this.game.debug.text("", 32, 660, "yellow", "Segoe UI");
   },
 
@@ -181,7 +196,10 @@ var PlayScene = {
   endPause: function() {
     //Hace transparente el menu de pausa
     this.Pause.alpha = 0;
-  }
+  },
+
 };
+
+
 
 module.exports = PlayScene;
