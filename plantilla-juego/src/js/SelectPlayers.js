dc60e83c994fb4668b76e06a210b57ccc2e7dd7b
jossan10 <jossan10@ucm.es>
2019-01-06 00:31:10 +0100

    Reedicion Estados Menu y SelectPlayer
    Los estados están mal diseñados. Se utilizan var de ámbito, cuando hay que usar atributos propios.
    
    No se define this.game como el parámetro de entrada, por lo que no se podía diseñar la ventana y la cámara.
    
    se reestructura el código y se trabajará en nuevas imágenes para los menus.
    
    Los botones igual. No están bien diseñados, y por eso no se implementan bien con la ventana 800x800
    
    se suprime el border line establecido en lenguaje de etiquetas HTML para dar paso a la imagen del HUD, dónde se redenerizará PhaserTEXT.

--- plantilla-juego/src/js/SelectPlayers.js
+++ plantilla-juego/src/js/SelectPlayers.js
@@ -3,21 +3,12 @@
 var SelectPlayers =  {
     
     create:function (game) {
-
-        var playersSelection;
+ 
+        this.playersSelection = game.add.sprite(400, 500, 'FondoSelectPlayers');
         
-        var B1;
-        var B2;
-        var B3;
-        
-
-      
-        playersSelection = game.add.sprite(400, 500, 'FondoSelectPlayers');
-        playersSelection.anchor.setTo(0.5,0.5);
-        
-        B1 = game.add.button(50, 400, 'Button1', this.functButton1, this, 2, 1, 0);
-        B2 = game.add.button(300, 400, 'Button2', this.functButton2, this, 2, 1, 0);
-        B3 = game.add.button(550, 400, 'Button2', this.functButton3, this, 2, 1, 0);
+        this.B1 = game.add.button(50, 400, 'Button1', this.functButton1, this, 2, 1, 0);
+        this.B2 = game.add.button(300, 400, 'Button2', this.functButton2, this, 2, 1, 0);
+        this.B3 = game.add.button(550, 400, 'Button3', this.functButton3, this, 2, 1, 0);
         
     },
 
--- plantilla-juego/src/js/StartScreen.js
+++ plantilla-juego/src/js/StartScreen.js
@@ -4,23 +4,25 @@ var startScreen =  {
     
     create:function (game) {
 
-        var MainMenu;
-        var Music;
+        this.game = game;
+        this.game.world.setBounds(0, 0, 800, 800);
+        this.MainMenu = game.add.sprite(0, 0, 'Menu');
+        this.Music =  game.add.audio('AudioMenu',0.4, true);
         this.cursor = this.game.input.keyboard;
+        //input 
+        this.key1 = Phaser.KeyCode.ENTER;
 
-      
-        MainMenu = game.add.sprite(400, 500, 'Menu');
-        MainMenu.anchor.setTo(0.5,1);
-        this.Music = game.add.audio('AudioMenu',0.4, true);
-       
+        this.MainMenu.width = 800;
+        this.MainMenu.height = 800;
+
         this.Music.play();
       
         
     },
 
     update:function (game) {
-        if (this.cursor.isDown(13)) {
-            //this.Music.mute = true;
+        if (this.cursor.isDown(this.key1)) {
+
             this.Music.stop();
 
             this.state.start('SPScene');
