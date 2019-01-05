2371e50324c91e8bd8d8f4fa8693c599c85db942
dagil02 <dagil02@ucm.es>
2019-01-05 11:38:34 +0100

    Merge branch 'joseS'
    merge hecho a mano

--- plantilla-juego/src/js/play_scene.js
+++ plantilla-juego/src/js/play_scene.js
@@ -68,9 +68,12 @@ var PlayScene = {
   },
 
   update: function() {
+   
     if (!this.pause) {
-      this.checkInput();
-      this.playerGroup.children[0].bulletUpdate();
+      this.checkInput(); //gestiona el input de cada jugador en su turno
+      this.playerGroup.children[0].bulletUpdate(this.mapa);  
+    
+       //gestiona las colisiones de balas y su llamada a destrucción
     } else {
       //La tecla enter manda al menu inicial
       if (this.inputAux.isDown(13)) {
@@ -88,7 +91,7 @@ var PlayScene = {
     var y = 38;
 
     this.game.debug.text(
-      `Debugging object: PLAYER TEST`,
+      `PLAYER TESTING: `,
       32,
       610,
       "yellow",
@@ -103,6 +106,25 @@ var PlayScene = {
       "yellow",
       "Segoe UI"
     );
+
+    if (this.playerGroup.children[0].currentWeapon){
+      this.game.debug.text(
+        `Debugging object: CURRENT WEAPON: ` +  this.playerGroup.children[0].currentWeapon.tipoArma,
+        350,
+        610,
+        "yellow",
+        "Segoe UI"
+      );
+      this.game.debug.text(
+        "balas: " + this.playerGroup.children[0].currentWeapon.balas_Cont + " damage: " + 
+        this.playerGroup.children[0].currentWeapon.damage + " alcance: " + this.playerGroup.children[0].currentWeapon.alcance,
+        350,
+        640,
+        "yellow",
+        "Segoe UI"
+      );
+    }
+    
     //this.game.debug.text("", 32, 660, "yellow", "Segoe UI");
   },
 
@@ -181,7 +203,10 @@ var PlayScene = {
   endPause: function() {
     //Hace transparente el menu de pausa
     this.Pause.alpha = 0;
-  }
+  },
+
 };
+
+
 
 module.exports = PlayScene;
