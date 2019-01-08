adf3c578ad4e421287d0336d21c6829c2aec42ea
dagil02 <dagil02@ucm.es>
2019-01-08 11:26:42 +0100

    otro intento de arreglar el video

--- js/bundle.js
+++ js/bundle.js
@@ -213,15 +213,15 @@ var introScene = {
     //this.video = this.game.video.create();
     this.video = this.game.add.video('introScene');
     this.video.addToWorld(450, 400, 0.5, 0.5, 0.5, 0.5);
-    this.video.play();
+    
 
     this.videoOFF = new Phaser.Signal();
     this.video.onComplete = this.videoOFF;
     this.videoOFF.addOnce(this.changeState,this);
     
-   
-
-   
+   if(this.video != undefined){
+    this.video.play();
+   }
   },
   update: function(game) {
   },
